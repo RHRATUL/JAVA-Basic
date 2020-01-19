5d4452b6dd57b7c52f9b77523d695506b0d9dd87
RATUL <RATUL@DESKTOP-I22IK3L>
2020-01-20 00:12:33 +0600

    4th commit

--- Learnjava/src/basic/Switch.java
+++ Learnjava/src/basic/Switch.java
@@ -5,40 +5,36 @@ import java.util.Scanner;
 public class Switch {
 
 	public static void main(String[] args) {
-	int num;
-	Scanner input = new Scanner(System.in);
-	  System.out.println("Enter a Number");
-num=input.nextInt();
+		int num;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a Number");
+		num = input.nextInt();
 
-switch(num)
-{ 
-case 1:
-	System.out.println("This is 1");
-	break;
-case 2:
-	System.out.println("This is 2");
-	break;
-case 3:
-	System.out.println("This is 3");
-	break;
-case 4:
-	System.out.println("This is 4");
-	break;
-	
-	
-case 5:
-	System.out.println("This is 5");
-	break;
-	case 6:
-		System.out.println("This is 6");
-		break;
-		
-	default:
-		System.out.println("Invalid input");
+		switch (num) {
+		case 1:
+			System.out.println("This is 1");
+			break;
+		case 2:
+			System.out.println("This is 2");
+			break;
+		case 3:
+			System.out.println("This is 3");
+			break;
+		case 4:
+			System.out.println("This is 4");
+			break;
 
-		
-	
-}
+		case 5:
+			System.out.println("This is 5");
+			break;
+		case 6:
+			System.out.println("This is 6");
+			break;
+
+		default:
+			System.out.println("Invalid input");
+
+		}
 
 	}
 
