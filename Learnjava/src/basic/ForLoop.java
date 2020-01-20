49954dab0f5e01cf31d15c4e66fa643f19741785
RATUL <RATUL@DESKTOP-I22IK3L>
2020-01-21 00:32:31 +0600

    7th commit

---
+++ Learnjava/src/basic/ForLoop.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class ForLoop {
+	public static void main(String args[]) {
+		int a, i;
+		for (i = 0; i < 100; i++) {
+			System.out.println(i);
+		}
+		for(i=0;i<100;i++)
+		{
+			System.out.println("Bangladesh");
+			
+		}
+		for(i=2;i<100;i=i+2)
+		{
+			System.out.println(i);
+		}
+	}
+}
